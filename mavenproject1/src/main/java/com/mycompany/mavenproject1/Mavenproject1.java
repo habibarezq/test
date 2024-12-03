ef07a0986456b78443715112d49ce06e00ea42fa
habibaelghazouly <habibahoussam7@gmail.com>
2024-12-03 17:55:20 +0200

    added habibaelghazouly

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,8 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+      
+        habibaelghazouly h = new habibaelghazouly();
+        System.out.println("number is " + h.num);
     }
 }
--- mavenproject1/src/main/java/com/mycompany/mavenproject1/habibaelghazouly.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/habibaelghazouly.java
@@ -2,11 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+package com.mycompany.mavenproject1;
 
 /**
  *
  * @author Habiba Elghazouly
  */
-public class habibaelghaazouly {
+public class habibaelghazouly {
+    public int num ;
     
+    public habibaelghazouly(){
+        num = 2;
+    }
 }
