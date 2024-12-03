b5e34aceb491a367f41656ff2cc4bd76736c8185
JanaEmad04 <ejana7358@gmail.com>
2024-12-03 18:07:46 +0200

    changed print

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,6 +11,6 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!   Jana");
+        System.out.println("Hello Im jana!");
     }
 }
