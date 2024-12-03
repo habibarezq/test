e89da73bbf3cdf47bf133a8450d0a206824e26e0
Habiba abdel hameed <154561371+habibarezq@users.noreply.github.com>
2024-12-03 17:52:20 +0200

    habiba rezq

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -4,6 +4,8 @@
 
 package com.mycompany.mavenproject1;
 
+import newpackage.NewClasshabiba;
+
 /**
  *
  * @author habib
@@ -12,5 +14,7 @@ public class Mavenproject1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        NewClasshabiba h=new NewClasshabiba("Habiba Rezq");
+        h.print();
     }
 }
---
+++ mavenproject1/src/main/java/newpackage/NewClasshabiba.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package newpackage;
+
+/**
+ *
+ * @author habib
+ */
+public class NewClasshabiba {
+    private String name;
+
+    public NewClasshabiba(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void print()
+    {
+        System.out.println("Hellooo "+name);
+    }
+}
